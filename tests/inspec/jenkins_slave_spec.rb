9e65a11b278ee1c655bf1af22b00d5a39d04609e
diodonfrost <antoine.mace@protonmail.com>
2018-10-16 00:09:06 +0200

    add basic images with systemd integration

--- tests/inspec/jenkins_slave_spec.rb
+++ tests/inspec/jenkins_slave_spec.rb
@@ -11,3 +11,23 @@ control 'jenkins-slave-01' do
     it { should be_installed }
   end
 end
+
+control 'jenkins-slave-02' do
+  impact 1.0
+  title 'Jenkins jar'
+  desc 'Jenkins jar should be present'
+  describe file('/usr/share/jenkins/slave.jar') do
+    it { should exist }
+    its('mode') { should cmp '0644' }
+  end
+end
+
+control 'jenkins-slave-03' do
+  impact 1.0
+  title 'Jenkins slave'
+  desc 'Jenkins slave binary should be present'
+  describe file('/usr/local/bin/jenkins-slave') do
+    it { should exist }
+    it { should be_executable }
+  end
+end
